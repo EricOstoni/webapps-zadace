2213a8a16bc0831beea5328d1a6f083db4574bed
ericostoni <eostoni@student.unipu.hr>
2022-10-11 04:35:00 +0200

    Riješen zadatak 4

---
+++ 01-zadaca/zadatak04.js
@@ -0,0 +1,13 @@
+function divWiththree () {
+    Array = [1,2,3,4,5,6,7,8,9,10] 
+    for (count=0; count<Array.length; count++) {
+        if (Array[count]%3==0) 
+        { console.log(Array[count]) } 
+    } 
+    
+
+    
+
+}
+
+divWiththree() 
