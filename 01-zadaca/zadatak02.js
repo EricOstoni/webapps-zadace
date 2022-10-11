4862f519274f48fcbc4ff21405166193de084af8
ericostoni <eostoni@student.unipu.hr>
2022-10-11 04:08:56 +0200

    Riješen zadatak 2

---
+++ 01-zadaca/zadatak02.js
@@ -0,0 +1,9 @@
+function searchNumber (x) {
+    
+    if (x <=100 || x>=150000 ) {console.log("->" + x + " nije unutar [100,150000]")}
+        else {console.log("->"+ x + " unutar je [100,150000]" )}
+    
+}  
+
+
+searchNumber(53)    
